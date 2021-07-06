cabb9f40127536ffd33386446a6b2d25bb07e91f
Bowen <37032357+Owenll66@users.noreply.github.com>
2021-07-06 19:12:47 +0930

    Update MergeSort.java

--- Sorting Algorithms/MergeSort.java
+++ Sorting Algorithms/MergeSort.java
@@ -4,7 +4,7 @@ public class MergeSort
     *INPUT: arr -- the array to merge
     *       left -- the left element index (usually 0 at the start)
     *       middle -- the middle element index ((left + right) / 2)
-    *       right -- the right element index (usually arr.lenth-1 at the start)
+    *       right -- the right element index (usually arr.lenth-1 at the end)
     */
     public static void merge(int arr[], int left, int middle, int right) 
     { 
@@ -61,7 +61,7 @@ public class MergeSort
     /*
     *INPUT: arr -- the array need to sort
     *       left -- the left element index (usually 0 at the start)
-    *       right -- the right element index (usually arr.lenth-1 at the start)
+    *       right -- the right element index (usually arr.lenth-1 at the end)
     */
     static void mergeSort(int arr[], int left, int right) 
     { 

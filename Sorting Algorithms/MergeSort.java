2ff9c142cabefcc3c1acd1a8133232d29d54802e
Bowen Liu <592623044@qq.com>
2022-02-07 23:11:26 +1030

    Update MergeSort.java

--- Sorting Algorithms/MergeSort.java
+++ Sorting Algorithms/MergeSort.java
@@ -1,32 +1,33 @@
+import java.util.Arrays;
+
 public class MergeSort
 {
    /*
-    *INPUT: arr -- the array to merge
-    *       firstIndex -- the first element index (usually 0 at the start)
-    *       middle -- the middle element index ((left + right) / 2)
-    *       rightIndex -- the last element index (usually arr.lenth-1 at the end)
+    * INPUT: arr -- the array to merge
+    *        firstIndex -- the first element index (usually 0 at the start)
+    *        middleIndex -- the middle element index ((left + right) / 2)
+    *        rightIndex -- the last element index (usually arr.lenth-1 at the end)
     */
-    public static void merge(int arr[], int firstIndex, int middle, int lastIndex) 
+    public static void merge(int arr[], int firstIndex, int middleIndex, int lastIndex) 
     { 
-        int leftArrayLength = middle - firstIndex + 1; 
-        int rightArrayLength =  lastIndex - middle; 
-      
-        int[] L = new int[leftArrayLength]; 
-        int[] R = new int[rightArrayLength]; 
+        int leftArrayLength = middleIndex - firstIndex + 1; 
+        int rightArrayLength =  lastIndex - middleIndex; 
+
+        // Copy the left half to arr L
+        int[] L = Arrays.copyOfRange(arr, firstIndex, middleIndex + 1);
+
+        // Copy the right half to arr R
+        int[] R = Arrays.copyOfRange(arr, middleIndex + 1, lastIndex + 1); 
+
         
-        //copy the left half to arr L
-        for (int i = 0; i < leftArrayLength; i++) 
-            L[i] = arr[firstIndex + i];
-       
-        //copy the right half to arr R
         for (int i = 0; i < rightArrayLength; i++) 
-            R[i] = arr[middle + 1+ i]; 
+            R[i] = arr[middleIndex + 1+ i]; 
       
         int i = 0;
         int j = 0;
         int k = firstIndex;
         
-        //sort array from the smallest to the largest
+        // Sort array from the smallest to the largest
         while (i < leftArrayLength && j < rightArrayLength) 
         { 
             if (L[i] <= R[j]) 
@@ -43,7 +44,7 @@ public class MergeSort
             k++; 
         } 
       
-        //add the remaining elements from arr L to the end
+        // Add the remaining elements from arr L to the end
         while (i < leftArrayLength) 
         { 
             arr[k] = L[i]; 
@@ -51,7 +52,7 @@ public class MergeSort
             k++; 
         } 
       
-        //add the remaining elements from arr R to the end
+        // Add the remaining elements from arr R to the end
         while (j < rightArrayLength) 
         { 
             arr[k] = R[j]; 
@@ -60,9 +61,9 @@ public class MergeSort
         }
     }
     /*
-    *INPUT: arr -- the array need to sort
-    *       first -- the first element index (usually 0 at the start)
-    *       last -- the last element index (usually arr.lenth-1 at the end)
+    * INPUT: arr -- the array need to sort
+    *        first -- the first element index (usually 0 at the start)
+    *        last -- the last element index (usually arr.lenth-1 at the end)
     */
     static void mergeSort(int arr[], int firstIndex, int lastIndex) 
     { 
@@ -80,32 +81,33 @@ public class MergeSort
     {
         int[] test1 = {1, 3 ,8 ,9, 7, 5, 4, 2, 6};
         int[] test2 = {213, 4622378, 723298, 4654564, 1, 2, 3213,4343};
+
+        // Test1
         System.out.print("Test1:\n");
-        for(int i = 0; i < test1.length; i++)
-        {
-          System.out.print(" "+test1[i]);
-        }
+        printArray(test1);
         System.out.println();
         mergeSort(test1, 0, test1.length - 1);
         System.out.print("Result after mergeSort:\n");
-        for(int i = 0; i < test1.length; i++)
-        {
-          System.out.print(" "+test1[i]);
-        }
-        System.out.println();
-        System.out.println();
-        //Test2
+        printArray(test1);
+        System.out.println("\n");
+
+        // Test2
         System.out.print("Test2:\n");
-        for(int i = 0; i < test2.length; i++)
-        {
-          System.out.print(" "+test2[i]);
-        }
+        printArray(test2);
         System.out.println();
         mergeSort(test2, 0, test2.length - 1);
         System.out.print("Result after mergeSort:\n");
-        for(int i = 0; i < test2.length; i++)
-        {
-          System.out.print(" "+test2[i]);
+        printArray(test2);
+    }
+
+    public static void printArray(int[] arr)
+    {
+        System.out.print("[ ");
+
+        for (int i : arr) {
+            System.out.print(i + " ");
         }
+
+        System.out.print("]");
     }
 }
